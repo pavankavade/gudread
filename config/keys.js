37579dd6add59799fc9c7a1344d1c0b4927f9511
Pavan Kavade <rorchach1997@gmail.com>
2018-12-28 22:16:58 +0530

    handle auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://pavan1:pavan1@ds125322.mlab.com:25322/storybooks-dev1',
+  googleClientID: '349047710265-hn49o5k2qr7cpgf9o1kdbl5uc2lbapsi.apps.googleusercontent.com', 
+  googleClientSecret:'inoQzmTO4QKN49smQIku5AxA'
+}
