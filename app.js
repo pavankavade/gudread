65e2fd44459dc56c3d09dc4221f9e1e14979c191
Pavan Kavade <rorchach1997@gmail.com>
2019-01-02 22:47:26 +0530

    version alpha 1.0

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 const exphbs = require('express-handlebars');
-
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 //Load user model
 require('./models/User');
-
+require('./models/Story');
 //Passport config
 require('./config/passport')(passport);
 
 //Load Routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
-
+const stories = require('./routes/stories');
 //Load Keys
-
 const keys = require('./config/keys');
+
+//HandleBarss helpers
+const{
+  truncate,
+  stripTags,
+  formatDate,
+  select,
+  editIcon
+} = require('./helpers/hbs');
 
 //Map global promises
 mongoose.Promise = global.Promise;
@@ -33,8 +43,22 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+
+//Body Parser Middleware)
+app.use(bodyParser.urlencoded({ extended:false}))
+app.use(bodyParser.json())
+
+//medthod override middleware
+app.use(methodOverride('_method'))
 //Handlebar middleware
 app.engine('handlebars', exphbs({
+  helpers: {
+    truncate:truncate,
+    stripTags:stripTags,
+    formatDate: formatDate,
+    select:select,
+    editIcon:editIcon
+  },
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
@@ -55,9 +79,13 @@ app.use((req,res,next)=>{
   next();
 });
 
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Use routes
 app.use('/auth',auth);
 app.use('/', index);
+app.use('/stories',stories);
 
 const port = process.env.PORT || 5000;
 
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-module.exports = {
-  mongoURI: 'mongodb://pavan1:pavan1@ds125322.mlab.com:25322/storybooks-dev1',
-  googleClientID: '349047710265-hn49o5k2qr7cpgf9o1kdbl5uc2lbapsi.apps.googleusercontent.com', 
-  googleClientSecret:'inoQzmTO4QKN49smQIku5AxA'
+if(process.env.NODE_ENV ==='production'){
+  module.exports = require('./keys_prod');
+
+}else{
+  module.exports = require('./keys_dev');
 }
